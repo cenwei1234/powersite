faa92983b80cf990144a35acaac20f1cc16a7e7e
hu ming <1250446609@qq.com>
2019-12-18 10:28:16 +0800

    20191218   加了deviceconfig deviceoperation的版本

--- src/main/java/com/jialong/powersite/modular/system/model/SiteOperationAddDeviceData.java
+++ src/main/java/com/jialong/powersite/modular/system/model/SiteOperationAddDeviceData.java
@@ -1,20 +1,24 @@
 package com.jialong.powersite.modular.system.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.jialong.powersite.modular.system.model.request.SiteOperationAddParamData;
 
 import java.util.List;
 
 public class SiteOperationAddDeviceData {
 
+    @JsonProperty(value = "deviceType")
     private Integer deviceId;
 
     private String deviceName;
 
     private Integer deviceStatus;
 
-    private Integer deviceType;
-
     private List<SiteOperationAddParamData> paramDataList;
+
+    private List<JlDeviceConfig> configList;
+
+    private List<JlDeviceOperation> operationList;
 
     public Integer getDeviceId() {
         return deviceId;
@@ -40,19 +44,27 @@ public class SiteOperationAddDeviceData {
         this.deviceStatus = deviceStatus;
     }
 
-    public Integer getDeviceType() {
-        return deviceType;
-    }
-
-    public void setDeviceType(Integer deviceType) {
-        this.deviceType = deviceType;
-    }
-
     public List<SiteOperationAddParamData> getParamDataList() {
         return paramDataList;
     }
 
     public void setParamDataList(List<SiteOperationAddParamData> paramDataList) {
         this.paramDataList = paramDataList;
+    }
+
+    public List<JlDeviceConfig> getConfigList() {
+        return configList;
+    }
+
+    public void setConfigList(List<JlDeviceConfig> configList) {
+        this.configList = configList;
+    }
+
+    public List<JlDeviceOperation> getOperationList() {
+        return operationList;
+    }
+
+    public void setOperationList(List<JlDeviceOperation> operationList) {
+        this.operationList = operationList;
     }
 }
